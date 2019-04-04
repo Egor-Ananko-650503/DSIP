a5b919b06f291938f84304245abcd89baef14935
Svetlana Reznichenko <srezrez@gmail.com>
2019-04-05 02:59:42 +0300

    A class for file writting was added

--- Lab 1/src/by/bsuir/dsip/Main.java
+++ Lab 1/src/by/bsuir/dsip/Main.java
@@ -1,11 +1,8 @@
 package by.bsuir.dsip;
 
+import by.bsuir.dsip.action.FileAction;
 import by.bsuir.dsip.action.Fourier;
 import by.bsuir.dsip.bean.Complex;
-
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 //y = cos(3x) + sin(2x) N = 8
 public class Main {
@@ -14,63 +11,33 @@ public class Main {
         final double T = 1;
         final double fd = N / (2 * T);
         Complex[] x = new Complex[N];
+        FileAction fileAction = new FileAction();
+        final int NN = 128;
 
-        try {
-            FileWriter fileWriter = new FileWriter("source.txt");
-            PrintWriter printWriter = new PrintWriter(fileWriter);
-            for (int i = 0; i < 128; i++) {
-                printWriter.printf("%s, %s\n", i, Math.cos(3 * i * 0.125) + Math.sin(2 * i * 0.125));
-            }
-            printWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        Complex[] source = new Complex[NN];
+        for (int i = 0; i < source.length; i++) {
+            source[i] = new Complex(Math.cos(3 * i * 0.125) + Math.sin(2 * i * 0.125), 0.0);
         }
+        fileAction.writeInRe(source, "source.txt");
 
-        try {
-            FileWriter fileWriter = new FileWriter("before.txt");
-            PrintWriter printWriter = new PrintWriter(fileWriter);
-            for (int i = 0; i < N; i++) {
-                x[i] = new Complex(Math.cos(3 * i) + Math.sin(2 * i), 0.0);
-                printWriter.printf("%s\n", x[i].re());
-            }
-            printWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (int i = 0; i < N; i++) {
+            x[i] = new Complex(Math.cos(3 * i) + Math.sin(2 * i), 0.0);
         }
+        fileAction.writeInRe(x, "before.txt");
 
         Complex[] dx = Fourier.fft(x);
 //        Complex[] rdx = dx;
+
         Complex[] rdx = new Complex[dx.length];
         for (int i = 0; i < dx.length / 2; i++) {
             rdx[dx.length / 2 - 1 - i] = dx[i];
             rdx[dx.length - 1 - i] = dx[dx.length / 2 + i];
         }
-        try {
-            FileWriter fileWriter = new FileWriter("ampl.txt");
-            PrintWriter printWriter = new PrintWriter(fileWriter);
-            FileWriter fileWriterP = new FileWriter("phase.txt");
-            PrintWriter printWriterP = new PrintWriter(fileWriterP);
-            for (int i = 0; i < rdx.length; i++) {
-                printWriter.printf("%s, %s\n", fd * ((double) i / N), rdx[i].abs());
-                printWriterP.printf("%s, %s\n", fd * ((double) i / N), rdx[i].phase());
-            }
-            printWriter.close();
-            printWriterP.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        fileAction.writeInAmpl(rdx, N, fd, "ampl.txt");
+        fileAction.writeInPhase(rdx, N, fd, "phase.txt");
 
         Complex[] idx = Fourier.ifft(dx);
-        try {
-            FileWriter fileWriter = new FileWriter("after.txt");
-            PrintWriter printWriter = new PrintWriter(fileWriter);
-            for (int i = 0; i < idx.length; i++) {
-                printWriter.printf("%s, %s\n", i, idx[i].re());
-            }
-            printWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        fileAction.writeInRe(idx, "after.txt");
 
         System.out.println("Done!");
     }
--- Lab 1/src/by/bsuir/dsip/action/FileAction.java
+++ Lab 1/src/by/bsuir/dsip/action/FileAction.java
@@ -1,4 +1,49 @@
 package by.bsuir.dsip.action;
 
+import by.bsuir.dsip.bean.Complex;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class FileAction {
+
+    private FileWriter fileWriter;
+    private PrintWriter printWriter;
+
+    private void initialize(String name){
+        try {
+            fileWriter = new FileWriter(name);
+            printWriter = new PrintWriter(fileWriter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeInRe(Complex[] x, String name){
+
+        initialize(name);
+        for(int i = 0; i < x.length; i++){
+            printWriter.printf("%s, %s\n", i, x[i].re());
+        }
+        printWriter.close();
+    }
+
+    public void writeInAmpl(Complex[] x, int N, double fd, String name){
+
+        initialize(name);
+        for (int i = 0; i < x.length; i++){
+            printWriter.printf("%s, %s\n", fd * ((double) i / N), x[i].abs());
+        }
+        printWriter.close();
+    }
+
+    public void writeInPhase(Complex[] x, int N, double fd, String name){
+
+        initialize(name);
+        for(int i = 0; i< x.length; i++){
+            printWriter.printf("%s, %s\n", fd * ((double) i / N), x[i].phase());
+        }
+        printWriter.close();
+    }
 }
